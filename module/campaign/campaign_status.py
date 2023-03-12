4e6185f0d4a40c87cdacefbdbd854453eeeccb73
chenqingyun <chenqingyun@mediway.cn>
2023-03-12 20:29:32 +0800

    初始化

---
+++ module/campaign/campaign_status.py
@@ -0,0 +1,153 @@
+import datetime
+import re
+
+import cv2
+import numpy as np
+
+from module.base.timer import Timer
+from module.campaign.assets import OCR_EVENT_PT, OCR_COIN, OCR_OIL, OCR_GEM, OCR_COIN_LIMIT, OCR_OIL_LIMIT
+from module.logger import logger
+from module.ocr.ocr import Ocr, Digit
+from module.ui.ui import UI
+from module.log_res.log_res import LogRes
+
+OCR_OIL = Digit(OCR_OIL, name='OCR_OIL', letter=(247, 247, 247), threshold=128)
+OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(239, 239, 239), threshold=128)
+OCR_OIL_LIMIT = Digit(OCR_OIL_LIMIT, name='OCR_OIL_LIMIT', letter=(235, 235, 235), threshold=128)
+OCR_COIN_LIMIT = Digit(OCR_COIN_LIMIT, name='OCR_COIN_LIMIT', letter=(239, 239, 239), threshold=128)
+
+
+class PtOcr(Ocr):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, lang='azur_lane', alphabet='X0123456789', **kwargs)
+
+    def pre_process(self, image):
+        """
+        Args:
+            image (np.ndarray): Shape (height, width, channel)
+
+        Returns:
+            np.ndarray: Shape (width, height)
+        """
+        # Use MAX(r, g, b)
+        r, g, b = cv2.split(cv2.subtract((255, 255, 255, 0), image))
+        image = cv2.min(cv2.min(r, g), b)
+        # Remove background, 0-192 => 0-255
+        image = cv2.multiply(image, 255 / 192)
+
+        return image.astype(np.uint8)
+
+
+OCR_PT = PtOcr(OCR_EVENT_PT)
+
+
+class CampaignStatus(UI):
+    def get_event_pt(self, update=False):
+        """
+        Returns:
+            int: PT amount, or 0 if unable to parse
+        """
+        pt = OCR_PT.ocr(self.device.image)
+
+        res = re.search(r'X(\d+)', pt)
+        if res:
+            pt = int(res.group(1))
+            logger.attr('Event_PT', pt)
+            LogRes(self.config).Pt = pt
+        else:
+            logger.warning(f'Invalid pt result: {pt}')
+            pt = 0
+        if update:
+            self.config.update()
+        return pt
+
+    def get_coin(self, skip_first_screenshot=True, update=False):
+        """
+        Returns:
+            int: Coin amount
+        """
+        _coin = {}
+        timeout = Timer(1, count=2).start()
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if timeout.reached():
+                logger.warning('Get coin timeout')
+                break
+
+            _coin = {
+                'Value': OCR_COIN.ocr(self.device.image),
+                'Limit': OCR_COIN_LIMIT.ocr(self.device.image)
+            }
+            if _coin['Value'] >= 100:
+                break
+        LogRes(self.config).Coin = _coin
+        if update:
+            self.config.update()
+
+        return _coin['Value']
+
+    def _watch_statistics_in_auto_search(self):
+        _coin = {
+            'Value': OCR_COIN.ocr(self.device.image),
+            'Limit': OCR_COIN_LIMIT.ocr(self.device.image)
+        }
+        _oil = {
+            'Value': OCR_OIL.ocr(self.device.image),
+            'Limit': OCR_OIL_LIMIT.ocr(self.device.image)
+        }
+        LogRes(self.config).Oil = _oil
+        LogRes(self.config).Coin = _coin
+        return _oil['Value']
+
+    def get_oil(self, skip_first_screenshot=True, update=False):
+        """
+        Returns:
+            int: Oil amount
+        """
+        _oil = {}
+        timeout = Timer(1, count=2).start()
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if timeout.reached():
+                logger.warning('Get oil timeout')
+                break
+
+            _oil = {
+                'Value': OCR_OIL.ocr(self.device.image),
+                'Limit': OCR_OIL_LIMIT.ocr(self.device.image)
+            }
+            if _oil['Value'] >= 100:
+                break
+        LogRes(self.config).Oil = _oil
+        if update:
+            self.config.update()
+
+        return _oil['Value']
+
+    def is_balancer_task(self):
+        """
+        Returns:
+             bool: If is event task but not daily event task
+        """
+        tasks = [
+            'Event',
+            'Event2',
+            'Raid',
+            'GemsFarming',
+        ]
+        command = self.config.Scheduler_Command
+        if command in tasks:
+            if self.config.Campaign_Event == 'campaign_main':
+                return False
+            else:
+                return True
+        else:
+            return False
---
+++ module/exercise/exercise.py
@@ -0,0 +1,182 @@
+import datetime
+from module.config.utils import get_server_last_update
+from module.exercise.assets import *
+from module.exercise.combat import ExerciseCombat
+from module.logger import logger
+from module.ocr.ocr import Digit, Ocr
+from module.ui.ui import page_exercise
+from module.config.utils import get_server_next_update
+
+OCR_EXERCISE_REMAIN = Digit(OCR_EXERCISE_REMAIN, letter=(173, 247, 74), threshold=128)
+OCR_EXERCISE_TIME_REMAIN = Ocr(buttons=OCR_EXERCISE_TIME_REMAIN, lang='cnocr', letter=(252, 253, 254),
+                               alphabet='天1234567890:')
+
+
+class Exercise(ExerciseCombat):
+    opponent_change_count = 0
+    remain = 0
+
+    def _new_opponent(self):
+        logger.info('New opponent')
+        self.appear_then_click(NEW_OPPONENT)
+        self.opponent_change_count += 1
+
+        logger.attr("Change_opponent_count", self.opponent_change_count)
+        self.config.set_record(Exercise_OpponentRefreshValue=self.opponent_change_count)
+
+        self.ensure_no_info_bar(timeout=3)
+
+    def _opponent_fleet_check_all(self):
+        if self.config.Exercise_OpponentChooseMode != 'leftmost':
+            super()._opponent_fleet_check_all()
+
+    def _opponent_sort(self, method=None):
+        if method is None:
+            method = self.config.Exercise_OpponentChooseMode
+        if method != 'leftmost':
+            return super()._opponent_sort(method=method)
+        else:
+            return [0, 1, 2, 3]
+
+    def _exercise_once(self):
+        """Execute exercise once.
+
+        This method handles exercise refresh and exercise failure.
+
+        Returns:
+            bool: True if success to defeat one opponent. False if failed to defeat any opponent and refresh exhausted.
+        """
+        self._opponent_fleet_check_all()
+        while 1:
+            for opponent in self._opponent_sort():
+                logger.hr(f'Opponent {opponent}', level=2)
+                success = self._combat(opponent)
+                if success:
+                    return success
+
+            if self.opponent_change_count >= 5:
+                return False
+
+            self._new_opponent()
+            self._opponent_fleet_check_all()
+
+    def _exercise_easiest_else_exp(self):
+        """Try easiest first, if unable to beat easiest opponent then switch to max exp opponent and accept the loss.
+
+        This method handles exercise refresh and exercise failure.
+
+        Returns:
+            bool: True if success to defeat one opponent. False if failed to defeat any opponent and refresh exhausted.
+        """
+        method = "easiest_else_exp"
+        restore = self.config.Exercise_LowHpThreshold
+        threshold = self.config.Exercise_LowHpThreshold
+        self._opponent_fleet_check_all()
+        while 1:
+            opponents = self._opponent_sort(method=method)
+            logger.hr(f'Opponent {opponents[0]}', level=2)
+            self.config.override(Exercise_LowHpThreshold=threshold)
+            success = self._combat(opponents[0])
+            if success:
+                self.config.override(Exercise_LowHpThreshold=restore)
+                return success
+            else:
+                if self.opponent_change_count < 5:
+                    logger.info("Cannot beat calculated easiest opponent, refresh")
+                    self._new_opponent()
+                    self._opponent_fleet_check_all()
+                    continue
+                else:
+                    logger.info("Cannot beat calculated easiest opponent, MAX EXP then")
+                    method = "max_exp"
+                    threshold = 0
+
+    def _get_opponent_change_count(self):
+        """
+        Same day, count set to last known change count or 6 i.e. no refresh
+        New day, count set to 0 i.e. can change up to 5 times
+
+        Returns:
+            int:
+        """
+        record = self.config.Exercise_OpponentRefreshRecord
+        update = get_server_last_update('00:00')
+        if record.date() == update.date():
+            # Same Day
+            return self.config.Exercise_OpponentRefreshValue
+        else:
+            # New Day
+            self.config.set_record(Exercise_OpponentRefreshValue=0)
+            return 0
+
+    def run(self):
+        server_update = self.config.Scheduler_ServerUpdate
+        hour_to_sec = 3600
+        sec_to_minutes = 60
+
+        self.ui_ensure(page_exercise)
+        # Only support cn now.
+        if self.config.SERVER == 'cn':
+            ocr = OCR_EXERCISE_TIME_REMAIN.ocr(self.device.image)
+            hour_current = datetime.datetime.now().hour
+
+            # If is at the last hours of the last day, empty remain.
+            if ('0天' in ocr or '天' not in ocr) and hour_current >= 18:
+                exercise_preserve = 0
+            else:
+                exercise_preserve = self.config.Exercise_ExercisePreserve
+
+            run = False
+            # Try once on the second Friday if chosen to.
+            if '2天' in ocr and hour_current >= 18 and self.config.Exercise_EmptyOnceOnSecondFriday:
+                exercise_preserve = 0
+                run = True
+        # Other servers.
+        else:
+            exercise_preserve = self.config.Exercise_ExercisePreserve
+            run = False
+
+        logger.attr('Times To Preserve', exercise_preserve)
+
+        # Delay task to the configured time
+        if ((get_server_next_update(server_update) - datetime.datetime.now()).seconds >
+            hour_to_sec * self.config.Exercise_DelayUntilHoursBeforeNextUpdate) \
+                and not run:
+            logger.warning(f'Exercise should run at {self.config.Exercise_DelayUntilHoursBeforeNextUpdate} '
+                           f'hours before next update. Delay task to it.')
+        else:
+            run = True
+
+        if run:
+            self.opponent_change_count = self._get_opponent_change_count()
+            logger.attr("Change_opponent_count", self.opponent_change_count)
+
+            while 1:
+                self.remain = OCR_EXERCISE_REMAIN.ocr(self.device.image)
+                if self.remain <= exercise_preserve:
+                    break
+                logger.hr(f'Exercise remain {self.remain}', level=1)
+                if self.config.Exercise_OpponentChooseMode == "easiest_else_exp":
+                    success = self._exercise_easiest_else_exp()
+                else:
+                    success = self._exercise_once()
+                if not success:
+                    logger.info('New opponent exhausted')
+                    break
+
+            # self.equipment_take_off_when_finished()
+
+        # Scheduler
+        with self.config.multi_set():
+            self.config.set_record(Exercise_OpponentRefreshValue=self.opponent_change_count)
+            if self.remain <= exercise_preserve or self.opponent_change_count >= 5:
+                next_run = get_server_next_update(server_update) \
+                           - datetime.timedelta(hours=self.config.Exercise_DelayUntilHoursBeforeNextUpdate)
+                now = datetime.datetime.now()
+                if next_run < now or run:
+                    self.config.task_delay(server_update=True)
+                    return
+                minutes_to_delay = int((next_run - now).total_seconds() / sec_to_minutes + 1)
+                self.config.task_delay(minute=minutes_to_delay)
+            else:
+                self.config.task_delay(success=False)
